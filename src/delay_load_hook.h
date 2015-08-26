f5a2bbd5d4f1336f0c419f07551e5163343a92c8
Justin Chase <justin@evovlehq.com>
2015-08-26 12:38:13 -0500

    More robust and safe delay load hook, based on PR https://github.com/nodejs/node-gyp/pull/653

--- src/delay_load_hook.h
+++ src/delay_load_hook.h
@@ -8,45 +8,51 @@
  */
 
 #ifdef _MSC_VER
- 
-// Add "#include <delay_load_hook.h>" to your addon
-// Then this to your modules bindings.gyp file:
-// 'conditions': [
-// ['OS=="win"', {
-//   'libraries': [
-//     '-lkernel32.lib',
-//     '-luser32.lib',
-//   ],
-//   'msvs_settings': {
-//     'VCLinkerTool': {
-//       'DelayLoadDLLs': ['node.dll', 'iojs.exe', 'node.exe'],
-//       'AdditionalOptions': ['/ignore:4199' ],
-//     }
-//   }
-// }],
-// ]
-// 
-// See this issue for more context:
-// https://github.com/iojs/io.js/issues/751
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <Shlwapi.h>
 #include <delayimp.h>
 #include <string.h>
 
-static FARPROC WINAPI load_exe_hook(unsigned int event, DelayLoadInfo* info)
-{
+static FARPROC WINAPI load_exe_hook(unsigned int event, DelayLoadInfo* info) {
+
   if (event != dliNotePreLoadLibrary)
     return NULL;
 
   if (_stricmp(info->szDll, "iojs.exe") != 0 &&
       _stricmp(info->szDll, "node.exe") != 0 &&
       _stricmp(info->szDll, "node.dll") != 0)
-  	return NULL;
+    return NULL;
 
-  HMODULE m = GetModuleHandle(info->szDll);
-  if (m == NULL) m = GetModuleHandle(NULL);
-  return (FARPROC) m;
+  // Get a handle to the current process executable.
+  HMODULE processModule = GetModuleHandle(NULL);
+
+  // Get the path to the executable.
+  TCHAR processPath[_MAX_PATH];
+  GetModuleFileName(processModule, processPath, _MAX_PATH);
+
+  // Get the name of the current executable.
+  LPSTR processName = PathFindFileName(processPath);
+
+  // If the current process is node or iojs, then just return the proccess module.
+  if (_stricmp(processName, "node.exe") == 0 ||
+      _stricmp(processName, "iojs.exe") == 0) {
+    return (FARPROC)processModule;
+  }
+
+  // If it is another process, attempt to load 'node.dll' from the same directory.
+  PathRemoveFileSpec(processPath);
+  PathAppend(processPath, "node.dll");
+
+  HMODULE nodeDllModule = GetModuleHandle(processPath);
+  if(nodeDllModule != NULL) {
+    // This application has a node.dll in the same directory as the executable, use that.
+    return (FARPROC)nodeDllModule;
+  }
+
+  // Fallback to the current executable, which must statically link to node.lib.
+  return (FARPROC)processModule;
 }
 
 PfnDliHook __pfnDliNotifyHook2 = load_exe_hook;
